5db04700a22f9744cd19bf233c1bfe507e79e2c3
Taner Özdemir <tanerozddev@gmail.com>
2024-05-27 17:58:13 +0300

    create: get Category questions fun

--- app/src/main/java/huahwei_project/quizapp/MainViewModel.kt
+++ app/src/main/java/huahwei_project/quizapp/MainViewModel.kt
@@ -16,6 +16,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     val categoriesLoad = MutableLiveData<Boolean>()
     val categoriesError = MutableLiveData<Boolean>()
 
+    val questionsData = MutableLiveData<List<Question>>()
+    val questionsLoad = MutableLiveData<Boolean>()
+    val questionsError = MutableLiveData<Boolean>()
+
     init {
         getCategoriesFromAPI()
     }
@@ -37,6 +41,28 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             override fun onFailure(call: Call<List<QuestionCategory>>, t: Throwable) {
                 categoriesLoad.value = false
                 categoriesError.value = true
+                Log.e("RetrofitError", t.message.toString())
+            }
+        })
+    }
+
+    fun getCategoryQuestions(categoryId: Int, amount: Int = 10) {
+        questionsLoad.value = true
+
+        questionAPI.getQuestions(categoryId = categoryId, amount = amount).enqueue(object : Callback<List<Question>> {
+            override fun onResponse(call: Call<List<Question>>, response: Response<List<Question>>) {
+                if (response.isSuccessful) {
+                    questionsData.value = response.body()
+                    questionsError.value = false
+                } else {
+                    questionsError.value = true
+                }
+                questionsLoad.value = false
+            }
+
+            override fun onFailure(call: Call<List<Question>>, t: Throwable) {
+                questionsLoad.value = false
+                questionsError.value = true
                 Log.e("RetrofitError", t.message.toString())
             }
         })
